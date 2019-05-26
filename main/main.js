5555902721a3ef8d56fdea8b3f0a4ab4f1b36b74
zhangwanyue <744404875@qq.com>
2019-05-26 17:52:34 +0800

    pass all test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    if(inputs.distance <= 2){
+        return Math.round(6 + inputs.parkTime*0.25);
+    }
+    if(inputs.distance > 2 && inputs.distance <=8){
+        return Math.round(6 + 0.8*(inputs.distance-2) + inputs.parkTime*0.25);
+    }
+    if(inputs.distance > 8){
+        return Math.round(6 + 0.8*(8-2) + 0.8*1.5*(inputs.distance-8) + inputs.parkTime*0.25);
+    }
 };
